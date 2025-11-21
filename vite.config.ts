3a73dd1f2f59479d9b8ddb37bfbb1581f68ba4f6
Exi Baclayon <ebenxh123@gmail.com>
2025-11-21 10:13:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: mode === 'production' ? '/actech-service-hub/' : './',
   server: {
     host: "::",
     port: 8080,
